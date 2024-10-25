6f6f41b90010c0c4ec8ce334f1d4b9aedf11f268
Michele <148053642+MicyHax@users.noreply.github.com>
2024-10-25 12:10:07 +0000

    modifiche

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cookieParser=require('cookie-parser');
 
 // Configurazione EJS
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 app.get('/', (req, res) => {
-    res.render('form');
-});
+    const name= req.cookies.name; //legge il cookie chiamato name
+    if(name){
+        //controlliamo se il cookie name esiste
+        res.render('greet',{message:'Ciao',name:name});
+    } else{
+        res.render('form'); //mostra il form se il cookie non esiste
+    }
+    }
+);
 
 app.post('/greet', (req, res) => {
     const name = req.body.name;
-    res.render('greet', { name: name });
+    res.cookie('name',name,{maxAge:24 * 60 * 60 * 1000  })
+    res.render('greet', {message:'Benvenuto', name: name });
 });
 
 const PORT = 3000;
